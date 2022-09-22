9d64a67638ae70359eed38504318bd6b0c1fa570
Viperack <theok04@gmail.com>
2022-09-22 09:38:37 +0200

    Theo work

--- WaveCrypt/main.c
+++ WaveCrypt/main.c
@@ -3,6 +3,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int littleEndian = 0x78563412;
+    int byteArray[sizeof(littleEndian)];
+    int bigEndian;
+
+    for (int i = (2 * sizeof(littleEndian)) - 1; i > 0; i -= 2) {
+        byteArray[i] = (littleEndian & (pow(16, i) + pow(16,i-1)));
+    };
+
+    for (int i = 0; i < sizeof(littleEndian); i++) {
+        bigEndian = (bigEndian | byteArray[i]);
+    };
+
+
+
+    char string[10];
+    sprintf(string, "%X", bigEndian);
+    printf(string);
     return 0;
 }
