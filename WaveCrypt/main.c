ac96b5018b85dda3ba84554affe9e9ea228ef3cd
Viperack <theok04@gmail.com>
2022-09-28 20:50:11 +0200

    Done with header reader function

--- WaveCrypt/main.c
+++ WaveCrypt/main.c
@@ -13,31 +13,31 @@ void printb(char val) {
   }
 }
 
-unsigned short chArLeToBeShort(unsigned char charArray[2]) {
+unsigned short charLEToShortBE(unsigned char charArray[2]) {
     return charArray[0] | charArray[1] << 8;
 }
 
-unsigned int chArLeToBeInt(unsigned char charArray[4]) {
+unsigned int charLEToIntBE(unsigned char charArray[4]) {
     return charArray[0] | charArray[1] << 8 | charArray[2] << 16 | charArray[3] << 24;
 }
 
-unsigned char * shortBeToChArLe (unsigned short num) {
+unsigned char * shortBEToCharLE (unsigned short num) {
     unsigned char* pCharArray = malloc(2);
 
     pCharArray[0] = num & 0xFF;
-    pCharArray[1] = num & 0x00FF;
+    pCharArray[1] = (num & 0xFF00) >> 8;
 
     return pCharArray;
 
 }
 
-unsigned char * intBeToChArLe (unsigned int num) {
+unsigned char * intBEToCharLE (unsigned int num) {
     unsigned char* pCharArray = malloc(4);
 
     pCharArray[0] = num & 0xFF;
-    pCharArray[1] = num & 0xFF00;
-    pCharArray[2] = num & 0xFF0000;
-    pCharArray[3] = num & 0xFF000000;
+    pCharArray[1] = (num & 0xFF00) >> 8;
+    pCharArray[2] = (num & 0xFF0000) >> 16;
+    pCharArray[3] = (num & 0xFF000000) >> 24;
 
     return pCharArray;
 }
@@ -53,14 +53,66 @@ struct HEADER headerToStruct(FILE* pWavOriginal) {
     int bytesRead = 0;
     struct HEADER header;
 
-    // RIFF 4 byte BE
-    bytesRead = fread(header.riff, sizeof(header.riff), 1, pWavOriginal);
-    printf("1 - 4: %s \n", header.riff);
+    // ChunkID  4 byte BE
+    bytesRead = fread(header.ChunkID, sizeof(header.ChunkID), 1, pWavOriginal);
+    printf("RIFF: %s \n", header.ChunkID);
 
-    // overall_size 4 byte LE
-    bytesRead = fread(buffer4[4], sizeof(buffer4[4]), 1, pWavOriginal);
+    // ChunkSize 4 byte LE converted to BE
+    bytesRead = fread(buffer4, sizeof(buffer4), 1, pWavOriginal);
+    header.ChunkSize = charLEToIntBE(buffer4);
+    printf("ChunkSize: %u \n", header.ChunkSize);
 
-    //
+    // Format 4 byte BE
+    bytesRead = fread(header.Format, sizeof(header.Format), 1, pWavOriginal);
+    printf("Format: %s \n", header.Format);
+
+    // Subchunk1ID 4 byte BE
+    bytesRead = fread(header.Subchunk1ID, sizeof(header.Subchunk1ID), 1, pWavOriginal);
+    printf("Subchunk1ID: %s \n", header.Subchunk1ID);
+
+    // Subchunk1Size 4 byte LE converted to BE
+    bytesRead = fread(buffer4, sizeof(buffer4), 1, pWavOriginal);
+    header.Subchunk1Size = charLEToIntBE(buffer4);
+    printf("Subchunk1Size: %u \n", header.Subchunk1Size);
+
+    // AudioFormat 2 byte LE converted to BE
+    bytesRead = fread(buffer2, sizeof(buffer2), 1, pWavOriginal);
+    header.AudioFormat = charLEToIntBE(buffer2);
+    printf("AudioFormat: %u \n", header.AudioFormat);
+
+    // NumChannels 2 byte LE converted to BE
+    bytesRead = fread(buffer2, sizeof(buffer2), 1, pWavOriginal);
+    header.NumChannels = charLEToIntBE(buffer2);
+    printf("NumChannels: %u \n", header.NumChannels);
+
+    // SampleRate 4 byte LE converted to BE
+    bytesRead = fread(buffer4, sizeof(buffer2), 1, pWavOriginal);
+    header.SampleRate = charLEToIntBE(buffer4);
+    printf("SampleRate: %u \n", header.SampleRate);
+
+    // ByteRate 4 byte LE converted to BE
+    bytesRead = fread(buffer4, sizeof(buffer4), 1, pWavOriginal);
+    header.ByteRate = charLEToIntBE(buffer4);
+    printf("ByteRate: %u \n", header.ByteRate);
+
+    // BlockAlign 2 byte LE converted to BE
+    bytesRead = fread(buffer2, sizeof(buffer2), 1, pWavOriginal);
+    header.BlockAlign = charLEToIntBE(buffer2);
+    printf("BlockAlign: %u \n", header.BlockAlign);
+
+    // BitsPerSample 2 byte LE converted to BE
+    bytesRead = fread(buffer2, sizeof(buffer2), 1, pWavOriginal);
+    header.BitsPerSample = charLEToIntBE(buffer2);
+    printf("BitsPerSample: %u \n", header.BitsPerSample);
+
+    // Subchunk2ID 4 byte BE
+    bytesRead = fread(header.Subchunk2ID, sizeof(header.Subchunk2ID), 1, pWavOriginal);
+    printf("Subchunk2ID: %s \n", header.Subchunk2ID);
+
+    // Subchunk2Size 4 byte LE converted to BE
+    bytesRead = fread(buffer4, sizeof(buffer4), 1, pWavOriginal);
+    header.Subchunk2Size = charLEToIntBE(buffer4);
+    printf("Subchunk2Size: %u \n", header.Subchunk2Size);
 
     fclose(pWavOriginal);
     return header;
@@ -181,34 +233,10 @@ int main(){
     //headerToStruct();
     //createNewWave();
 
-    unsigned char charArrayLittle[2] = {0xbb, 0xaa};
-    unsigned short shortBig = chArLeToBeShort(charArrayLittle);
-
-    unsigned char charArrayLittle4B[4] = {0xdd, 0xcc, 0xbb, 0xaa};
-    unsigned int intBig = chArLeToBeInt(charArrayLittle4B);
-
-    printf("ShortBig: %X\n", shortBig);
-    printf("IntBig: %X\n", intBig);
-
-    unsigned char * pCharArray2B = shortBeToChArLe(shortBig);
-    unsigned char * pCharArray4B = intBeToChArLe(intBig);
-
-    printf("pCharArray2B: ");
-    for (int i = 0; i < 2; i++) {
-        printf("%X", pCharArray2B[i]);
-    }
-    printf("\n");
-
-    printf("pCharArray4B: ");
-    for (int i = 0; i < 4; i++) {
-        printf("%X", pCharArray4B[i]);
-    }
-    printf("\n");
-
-    // menus();
+    //menus();
+    FILE* pWavOriginal = fopen("../AudioFiles/BabyElephantWalk60.wav", "rb");
+    struct HEADER header = headerToStruct(pWavOriginal);
 
 
-    free(pCharArray2B);
-    free(pCharArray4B);
     return 0;
 }
--- WaveCrypt/wave.h
+++ WaveCrypt/wave.h
@@ -3,17 +3,17 @@
 
 // WAVE file header format
 struct HEADER {
-    unsigned char riff[4];              // 4BE RIFF string
-    unsigned int overall_size;          // 4LE (file length - 8) bytes
-    unsigned char wave[4];              // 4BE WAVE string
-    unsigned char fmt_chunk_marker[4];  // 4BE fmt string with trailing null char
-    unsigned int length_of_fmt;         // 4LE length of remaining fmt data (should be 16 bytes)
-    unsigned short format_type;         // 2LE format type. 1=PCM, (3=IEEE float, 6=8bit A law, 7=8bit mu law)
-    unsigned short channels;            // 2LE no.of channels. 1=mono, 2=stereo
-    unsigned short sample_rate;         // 2LE sampling rate (blocks per second). ex. 44100
-    unsigned short byterate;            // 2LE bytes per second: SampleRate * NumChannels * BytesPerSample
-    unsigned short block_align;         // 2LE size of a block: NumChannels * BytesPerSample
-    unsigned short bits_per_sample;     // 2LE bits per sample, 8=8bits, 16=16 bits etc
-    unsigned char data_chunk_header [4];// 4BE DATA string
-    unsigned int data_size;             // 4LE data chunk size: NumSamples * NumChannels * BytesPerSample - (size of the next chunk that will be read)
+    unsigned char ChunkID[4];              // 4BE RIFF string
+    unsigned int ChunkSize;          // 4LE (file length - 8) bytes
+    unsigned char Format[4];              // 4BE WAVE string
+    unsigned char Subchunk1ID[4];  // 4BE fmt string with trailing null char
+    unsigned int Subchunk1Size;         // 4LE length of remaining fmt data (should be 16 bytes)
+    unsigned short AudioFormat;         // 2LE format type. 1=PCM, (3=IEEE float, 6=8bit A law, 7=8bit mu law)
+    unsigned short NumChannels;            // 2LE no.of channels. 1=mono, 2=stereo
+    unsigned int SampleRate;         // 4LE sampling rate (blocks per second). ex. 44100
+    unsigned int ByteRate;            // 2LE bytes per second: SampleRate * NumChannels * BytesPerSample
+    unsigned short BlockAlign;         // 2LE size of a block: NumChannels * BytesPerSample
+    unsigned short BitsPerSample;     // 2LE bits per sample, 8=8bits, 16=16 bits etc
+    unsigned char Subchunk2ID[4];// 4BE DATA string
+    unsigned int Subchunk2Size;             // 4LE data chunk size: NumSamples * NumChannels * BytesPerSample - (size of the next chunk that will be read)
 };
